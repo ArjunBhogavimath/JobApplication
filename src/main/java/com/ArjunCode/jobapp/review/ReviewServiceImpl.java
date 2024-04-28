78b08cc2ef344f84905adfff761b05a2a28d2960
Mallikarjunaiah B M <vpmallikarjuna03@gmail.com>
2024-04-29 01:57:40 +0530

    CRUD operations endpoints and logic implementation added for review.

--- src/main/java/com/ArjunCode/jobapp/review/ReviewServiceImpl.java
+++ src/main/java/com/ArjunCode/jobapp/review/ReviewServiceImpl.java
@@ -1,5 +1,7 @@
 package com.ArjunCode.jobapp.review;
 
+import com.ArjunCode.jobapp.company.Company;
+import com.ArjunCode.jobapp.company.CompanyService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -7,15 +9,73 @@ import java.util.List;
 @Service
 public class ReviewServiceImpl implements ReviewService{
 
-    private ReviewRepository reviewRepository;
+    private final ReviewRepository reviewRepository;
 
-    public ReviewServiceImpl(ReviewRepository reviewRepository) {
+
+    //need company service
+    private final CompanyService companyService;
+
+    public ReviewServiceImpl(ReviewRepository reviewRepository, CompanyService companyService) {
         this.reviewRepository = reviewRepository;
+        this.companyService = companyService;
     }
 
     @Override
     public List<Review> getAllReviews(Long companyId) {
         List<Review> reviews = reviewRepository.findByCompanyId(companyId);
         return reviews;
+    }
+
+    @Override
+    public boolean addReview(Long companyId, Review review) {
+        //need company data so created compayservice object
+        //so if we can find the company. we will save the company details in review and save review.
+        Company company = companyService.getCompany(companyId);
+        if(company != null){
+            review.setCompany(company);
+            reviewRepository.save(review);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public Review getReviewById(Long companyId, Long reviewId) {
+       //get all reviews for the particular company
+        //and filter it now using streams filter by reviewId
+        //return review or null
+        List<Review> reviews = reviewRepository.findByCompanyId(companyId);
+        return reviews.stream()
+                .filter(review -> review.getId().equals(reviewId))
+                .findFirst()
+                .orElse(null);
+    }
+
+    @Override
+    public boolean updateReview(Long companyId, Long reviewId, Review updatedReview) {
+        if(companyService.getCompany(companyId) != null){
+            updatedReview.setCompany(companyService.getCompany(companyId));
+            updatedReview.setId(reviewId);
+            reviewRepository.save(updatedReview);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean deleteReview(Long companyId, Long reviewId) {
+        //both company and review is bi directional mapping
+        // so if remove in 1 table, it wont be removed in other table
+        //so we need to remove it in company table and remove it in review table
+        if(companyService.getCompany(companyId) != null && reviewRepository.existsById(reviewId)){
+            Review review = reviewRepository.findById(reviewId).orElse(null);
+            Company company = review.getCompany();
+            company.getReviews().remove(review);
+          //  review.setCompany(null);
+            companyService.updateCompany(company,companyId);
+            reviewRepository.deleteById(reviewId);
+            return true;
+        }
+        return false;
     }
 }
